42efbec554db39814fb570b399e76807ee387a38
Angel Lopez <srterradev@gmail.com>
2024-10-21 18:48:27 -0600

    chore: Logos and adjustments for home

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,12 @@
+import Logo from "@/assets/sabana.png";
+import Image from "next/image";
+
 export const Navbar = () => {
   return (
     <div className={"w-full bg-transparent px-24 py-12 flex flex-col lg:flex-row items-center justify-between"}>
       <h1 className={"text-3xl font-bold text-red-400 text-center font-cinzel"}>Shaio Nurse</h1>
-      <div className={"flex text-gray-700 gap-12 mt-6 lg:mt-0"}>
-        <p className={"hover:opacity-45 transition cursor-pointer"}>Inicio</p>
-        <p className={"hover:opacity-45 transition cursor-pointer"}>Nosotros</p>
+      <div className={"flex text-gray-700 gap-12 lg:mt-0"}>
+        <Image src={Logo as any} className={"max-w-[200px]"} style={{ mixBlendMode: "multiply" }} alt={"Logo"}/>
       </div>
     </div>
   )
